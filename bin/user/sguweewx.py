802238e729958d76c6899c2e7b051467f7e05931
Shunra <43567521+swfrx@users.noreply.github.com>
2023-02-19 13:40:08 +0000

    Update sguweewx.py
    Now uses any HTM_ROOT rather than having it hard coded

--- bin/user/sguweewx.py
+++ bin/user/sguweewx.py
@@ -27,8 +27,8 @@ from weewx.cheetahgenerator import SearchList
 
 
 # Print version in syslog for easier troubleshooting
-VERSION = "1.0"
-loginf("version %s" % VERSION)
+VERSION = "2.0"
+loginf("sguweewx.py Version %s" % VERSION)
 
 
 class getData(SearchList):
@@ -49,21 +49,11 @@ class getData(SearchList):
         """
         Return a string that contains a DokuWiki table containing links
         to the NOAA Yearly & Monthly summaries
-
-        Variables:
-            noaa_directory  where the NOAA summary files are
-            noaa_namespace	the DokuWiki namespace they will be in
-            noaaa_year		the pattern to match for the names of the year summary files
-                month file names are assuming to have '-mm' ammended to the year number
-
-            these all have defaults but can be overriden in [Extras][sguweewx]
         """
 
 # Debugging?
         weewx.debug = int(self.generator.config_dict.get('debug', 0))
 
-        noaa_directory = self.generator.skin_dict['Extras'].get(
-            'sguweewx.noaa_directory', '/home/weewx/public_html/dokuwiki/pages/weather/noaa' )
         noaa_namespace = self.generator.skin_dict['Extras'].get(
             'sguweewx.noaa_namespace', 'weather:noaa:')
         noaa_year = self.generator.skin_dict['Extras'].get(
@@ -72,12 +62,27 @@ class getData(SearchList):
 # Month number & short name
         months=[('01','Jan'),('02','Feb'),('03','Mar'),('04','Apr'),('05','May'),('06','Jun'),('07','Jul'),('08','Aug'),('09','Sep'),('10','Oct'),('11','Nov'),('12','Dec')]
 
+# Find the right HTML ROOT
+        if "HTML_ROOT" in self.generator.skin_dict:
+            html_root = os.path.join(
+                self.generator.config_dict["WEEWX_ROOT"],
+                self.generator.skin_dict["HTML_ROOT"],
+            )
+        else:
+            html_root = os.path.join(
+                self.generator.config_dict["WEEWX_ROOT"],
+                self.generator.config_dict["StdReport"]["HTML_ROOT"],
+            )
+
+        noaa_directory = os.path.join( html_root , "dokuwiki", "pages", "weather", "noaa" )
+        loginf("noaa_directory = %s" % noaa_directory)
+
 # Directory with NOAA files becomes cwd
 # If this is a new installation the directory might not exist
 # so skip the following
         try:
             os.chdir( noaa_directory )
-        
+
 # Get names of the year files
             years =  glob.glob( noaa_year )
 
@@ -89,23 +94,23 @@ class getData(SearchList):
 # row by row using the correct newline for the system
                 noaa_table = ''
                 row = ''
-        
+
 # process years from current to oldest
                 for year in sorted(years, reverse = True):
                     yyyy = year.split('.')[0]
-                    year_name = yyyy.split('-')[1] 
-        
+                    year_name = yyyy.split('-')[1]
+
 # Start the table output for this year
                     row = self.do_link( noaa_namespace, year, year_name )
                     row = "|" + row
-            
+
 # Append the link to each month's file plus the short month name,
 # or a cell with just a hyphen if the month file doesn't exist
                     for month in months:
                         month_file = yyyy + '-' + month[0] + '.txt'
                         month_name = month[1]
                         row = row + self.do_link ( noaa_namespace, month_file, month_name )
-    
+
                     noaa_table = noaa_table + row + os.linesep
         except:
             noaa_table = 'NOAA directory ' + noaa_directory + ' does not exist'
@@ -119,4 +124,4 @@ class getData(SearchList):
             }
 
         # Finally, return our extension as a list:
-        return [search_list_extension] 
+        return [search_list_extension]
