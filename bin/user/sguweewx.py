d31fd2088e32830b612c0a381eee295208300f98
Shunra <43567521+swfrx@users.noreply.github.com>
2024-11-18 14:29:28 +0000

    Update sguweewx.py
    Version moved to 2.2d

--- bin/user/sguweewx.py
+++ bin/user/sguweewx.py
@@ -27,7 +27,7 @@ from weewx.cheetahgenerator import SearchList
 
 
 # Print version in syslog for easier troubleshooting
-VERSION = "2.2c"
+VERSION = "2.2d"
 loginf("sguweewx.py Version %s" % VERSION)
 
 
